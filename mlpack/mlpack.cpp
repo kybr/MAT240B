ec4de3d1805e9ae0ae9f0b862bbc781f74b05299
Karl Yerkes <karl.yerkes@gmail.com>
2020-03-05 05:36:40 -0800

    more clever

--- mlpack/mlpack.cpp
+++ mlpack/mlpack.cpp
@@ -2,11 +2,44 @@
 //
 // Example of using mlpack to do NN search of random data
 //
+#include <chrono>
 #include <iostream>
+#include <limits>
 #include <mlpack/methods/neighbor_search/neighbor_search.hpp>
 
-#include <chrono>
+// helper class for calculating statistics
+//
+template <class T>
+class MMM {
+  size_t size;
+  T sum{0};
+  T minimum{std::numeric_limits<T>::max()};
+  T maximum{-std::numeric_limits<T>::max()};
 
+ public:
+  void operator()(T value) {
+    ++size;
+    sum += value;
+    if (value < minimum) minimum = value;
+    if (value > maximum) maximum = value;
+  }
+  T mean() { return sum / size; }
+
+  void print() {
+    printf("min:%lf max:%lf mean:%lf\n", minimum, maximum, mean());
+  }
+  void csv() { printf("%lf,%lf,%lf", minimum, maximum, mean()); }
+};
+
+// helper functions for measuring time
+//
+auto tic() { return std::chrono::high_resolution_clock::now(); }
+double toc(std::chrono::high_resolution_clock::time_point then) {
+  return std::chrono::duration<double>(tic() - then).count();
+}
+
+// type of nearest neighbor search index
+//
 typedef mlpack::neighbor::NeighborSearch<   //
     mlpack::neighbor::NearestNeighborSort,  //
     mlpack::metric::EuclideanDistance,      //
@@ -14,7 +47,8 @@ typedef mlpack::neighbor::NeighborSearch<   //
     mlpack::tree::BallTree>                 //
     MyKNN;
 
-// tree types:
+// also try these:
+//
 // mlpack::tree::BallTree
 // mlpack::tree::KDTree
 // mlpack::tree::RStarTree
@@ -22,31 +56,37 @@ typedef mlpack::neighbor::NeighborSearch<   //
 // mlpack::tree::StandardCoverTree
 
 int main() {
-  int N = 100;
-  for (int d = 1; d < 32; d *= 2)
-    for (int n = pow(2, 8); n < pow(2, 15); n *= 2) {
+  int N = 1000;  // number of tries
+
+  for (int d = 1; d <= 32; d *= 2)
+    for (int n = pow(2, 8); n <= pow(2, 16); n *= 2) {
       arma::mat dataset(d, n, arma::fill::randu);
 
-      double mean = 0, minimum = 100, maximum = 0;
+      MMM<double> indexTime, queryTime;
+
       for (int i = 0; i < N; i++) {
         arma::mat query(d, 1, arma::fill::randu);
         arma::mat distances;
         arma::Mat<size_t> neighbors;
 
-        auto then = std::chrono::high_resolution_clock::now();
+        // build index
+        //
+        auto indexThen = tic();
         MyKNN myknn(dataset);
+        indexTime(toc(indexThen));
+
+        // execute query
+        //
+        auto queryThen = tic();
         myknn.Search(query, 1, neighbors, distances);
-
-        double t = std::chrono::duration<double>(
-                       std::chrono::high_resolution_clock::now() - then)
-                       .count();
-        if (t < minimum) minimum = t;
-        if (t > maximum) maximum = t;
-        mean += t;
+        queryTime(toc(queryThen));
       }
-      mean /= N;
 
-      printf("%d,%d,%f,%f,%f\n", d, n, minimum, mean, maximum);
+      printf("%d,%d,", d, n);
+      indexTime.csv();
+      printf(",");
+      queryTime.csv();
+      printf("\n");
     }
 }
 
