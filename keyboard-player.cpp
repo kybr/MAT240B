3349c5d689ed4fadc7ca313d5b9351732e100e59
Karl Yerkes <karl.yerkes@gmail.com>
2020-03-13 21:26:49 -0700

    fixed bug in keyboard thing

--- keyboard-player.cpp
+++ keyboard-player.cpp
@@ -19,9 +19,12 @@ typedef gam::SamplePlayer<float, gam::ipl::Cubic, gam::phsInc::Loop>
     SoundPlayer;
 
 struct MyApp : App {
+  bool update{false};
   int which{0};
-  float where{0};
-  mutex m;  // protects the two things above
+  float phase{0};
+  float period{0};
+  mutex m;  // protects the state above
+
   vector<SoundPlayer> playerList;
 
   void onInit() override {
@@ -49,17 +52,21 @@ struct MyApp : App {
     // get the lock; we just do nothing and move on. we'll do better next time.
     //
     if (m.try_lock()) {
-      // we (the audio thread) have the lock so we know that which and where
+      // we (the audio thread) have the lock so we know that our state
       // won't change until we release the lock.
       //
-      player = &playerList[which];
-      player->pos(where * player->frames());
+      if (update) {
+        update = false;
+        player = &playerList[which];
+        player->range(phase, period);
+        // printf("%d %f %f\n", which, player->pos(), player->period());
+      }
       m.unlock();
     }
 
     while (io()) {
       float f = player == nullptr ? 0.0f : player->operator()();
-      io.out(0) = io.out(1) = f;
+      io.out(0) = io.out(1) = f * 0.1;
     }
   }
 
@@ -67,8 +74,11 @@ struct MyApp : App {
     // protect the critical data with a lock
     //
     m.lock();
+    update = true;
     which = rnd::uniform(10);
-    where = rnd::uniform(1.0);
+    phase = rnd::uniform(1.0);
+    period = rnd::uniform(0.3);
+    // printf("%d %f %f\n", which, phase, period);
     m.unlock();
     return false;
   }
@@ -76,6 +86,6 @@ struct MyApp : App {
 
 int main() {
   MyApp app;
-  app.configureAudio(44100, 512, 2, 0);
+  app.configureAudio(44100, 1024, 2, 0);
   app.start();
 }
